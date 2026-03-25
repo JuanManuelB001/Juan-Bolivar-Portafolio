0bb7d3d17a79e07e42623a919da764990d4295b9
Juan Manuel Bolivar Mojica <jbolivarm2@ucentral.edu.co>
2026-03-25 18:04:40 -0500

    Create English.json and implement logic with LocalStorage

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
 
 
 //PONER CADA COMPONENTE DEL JSON
+// LOCALSTORAGE
+localStorage.setItem("lenguajes", true)
+let storage = JSON.parse(localStorage.getItem("lenguajes"));
 
-fetch("../data/dataEspanol.json").then((response) => response.json()).then( data => {
+if(storage == true){
+    fetch("../data/dataEspanol.json").then((response) => response.json()).then( data => {
     document.getElementById('titulo').textContent = data.titulo
 })
+}
+else{
+    fetch("../data/dataEnglish.json").then((response)=> response.json()).then((data) => {
+        document.getElementById("titulo").textContent = data.title
+    })
+    console.log("Ingles");
+}
