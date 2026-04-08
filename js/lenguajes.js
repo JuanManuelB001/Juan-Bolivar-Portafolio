437dbdd85ee82330fdb7b53ea1337742dbe26a62
Juan Manuel Bolivar Mojica <jbolivarm2@ucentral.edu.co>
2026-04-08 11:32:22 -0500

    frameworks and tools

--- js/lenguajes.js
+++ js/lenguajes.js
@@ -51,7 +51,11 @@ function getDataLenguajes(){
     list_batebase.forEach((element)=>{
         document.querySelector(".list-databases").innerHTML += `<ul><li class="profile-skill"><div  class="load-bar" ><div class="bar-percentage" style="width:${element[1]}%">${element[1]}%</div></div><span class="profile-skill-name"> ${element[0]}</span></li></ul>`
     })
-
+    // CAPTURARA INFORMACION JSON
+    list_tools=  Object.values(data.tools)
+    list_tools.forEach((element)=>{
+        document.querySelector(".tools-container").innerHTML+=`<span class="tool-tag">${element}</span>` 
+    })
 })
 }
 else{
